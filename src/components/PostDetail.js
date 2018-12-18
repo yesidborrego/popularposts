7c219d715545f356dc01a644872d272ad52e6d3e
Yesid Borrego <yesidborrego@gmail.com>
2018-12-17 23:42:50 -0500

    Add attribute rel to the posts links

--- src/components/PostDetail.js
+++ src/components/PostDetail.js
@@ -4,14 +4,16 @@ import {Table, Icon, Image} from 'semantic-ui-react';
 const PostDetail = ({post, handleUpVotes, handleDownVotes}) => {
   return (
     <Table.Row>
-      <Table.Cell><img src={post.post_image_url} alt={post.title} /></Table.Cell>
+      <Table.Cell>
+        <img src={post.post_image_url} alt={post.title} />
+      </Table.Cell>
       <Table.Cell textAlign='center'>
         <Icon link name='caret up' size='big' color='blue' onClick={handleUpVotes} />
         <strong>{post.votes}</strong><br/>
         <Icon link name='caret down' size='big' color='blue' onClick={handleDownVotes} />
       </Table.Cell>
       <Table.Cell>
-        <p><a href={post.url}>{post.title}</a></p>
+        <p><a href={post.url} target='_blank' rel="noopener noreferrer">{post.title}</a></p>
         <p>{post.description}</p>
         <span className='table_cell-writtenby'>Escrito por: </span><Image src={post.writer_avatar_url} alt='avatar' avatar />
       </Table.Cell>
