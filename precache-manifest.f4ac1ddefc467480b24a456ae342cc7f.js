31545a5fc45cfb73b206835eac621e471eecc386
Yesid Borrego <yesidborrego@gmail.com>
2018-12-18 01:17:45 -0500

    Updates

---
+++ precache-manifest.f4ac1ddefc467480b24a456ae342cc7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3bce4b34bb10d92cfe18",
+    "url": "/popularposts/static/js/runtime~main.3bce4b34.js"
+  },
+  {
+    "revision": "30497d8ad4cf76e8b253",
+    "url": "/popularposts/static/js/main.30497d8a.chunk.js"
+  },
+  {
+    "revision": "9baa0563c1236aa69514",
+    "url": "/popularposts/static/js/1.9baa0563.chunk.js"
+  },
+  {
+    "revision": "30497d8ad4cf76e8b253",
+    "url": "/popularposts/static/css/main.9b077cf6.chunk.css"
+  },
+  {
+    "revision": "40694ecb6aa639740ef59405e7909c2e",
+    "url": "/popularposts/index.html"
+  }
+];
